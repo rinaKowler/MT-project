e02ae69591f2c728675e35532535152dcf84955f
rina <rina351@gmail.com>
2021-12-24 11:02:34 +0200

    added somthing

--- website/urls.py
+++ website/urls.py
@@ -5,7 +5,7 @@ from website import views
 app_name = "website"
 
 urlpatterns = [
-    path("all_students/", views.get_all_students,name="students"),
+    path("all_students", views.get_all_students,name="students"),
     path("", views.home_page, name="home_page"),
     path("get_night_out/<str:trip_date>",views.show_night_out_arrangement, name = "night_out"),
    # path("get_all_payments/<str:amount",views.show_all_pament_payment, name = "payment"),
--- website/views.py
+++ website/views.py
@@ -1,14 +1,19 @@
+from os import name
+from django.core.exceptions import NON_FIELD_ERRORS
 from django.db.models.fields import NullBooleanField
 from django.shortcuts import render
 from django.http.response import HttpResponse
 from django.shortcuts import render, redirect
 from myapp.models import HotelName, NightOut, Payment, Students, Volunteer
+from django.db.models import Q
 
 
 def home_page(request):
     payment= Payment.objects.filter( paid=False)
+    paired = get_all_paired_students(1)
     return render(request, "website/home_page.html", {
             "payments": payment,
+            "paired": paired
 
     })
 
@@ -37,19 +42,21 @@ def set_hotel_arrengment(hotel_id):
     # todo:add room_students to db
     return rooms
 
-def  get_all_students():
-    return Students.objects.all()
+def  get_all_students(request):
+     return render (request,"website/students.html",{"name":name})
 
 def get_all_unpaired_students(student_id):
     return None
 #     return Students.objects.filter()
 
 def get_all_paired_students(student_id):
-    #all_rooms = NightOut.objects.filter(student.id = student_id)
-    #all_paired_students =[]
-    #for room in all_rooms:
-     #   students_in = NightOut.objects.filter(hotel_room_id = room.id)
-    return None
+    all_night_outs = NightOut.objects.filter(student__id = student_id)
+    all_paired_students =[]
+    for night_out in all_night_outs:
+        students_in = NightOut.objects.filter(hotel__id = night_out.hotel.id, trip_date = night_out.trip_date).exclude(student__id = night_out.student_id)
+        for paird in students_in:
+            all_paired_students.append(paird.student)
+    return set(all_paired_students)
         
 def get_all_hotel_rooms(hotel_name):
     return HotelName.objects.filter(name =hotel_name)
@@ -57,8 +64,7 @@ def get_all_hotel_rooms(hotel_name):
 def get_all_volunteer_places(volunteer_id):
     all_places = get_all_volunteer_places(volunteer_id)
     all_students=get_all_students()
-    if 
-     return Volunteer.objects.all()
+    return Volunteer.objects.all()
 
 
 
